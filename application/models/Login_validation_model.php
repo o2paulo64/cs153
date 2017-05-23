4094b4d46a057bbff2fdcad9e2872d49b170b733
paulo64 <pasantiago1.up.edu.ph>
2017-05-23 14:59:46 +0800

    again

--- application/models/Login_validation_model.php
+++ application/models/Login_validation_model.php
@@ -11,7 +11,7 @@ Class login_validation_model extends CI_Model
    $this -> db -> limit(1);
 
    $query = $this -> db -> get();
-   
+   print_r($query -> num_rows());
    
    if($query -> num_rows() == 1)
    {
